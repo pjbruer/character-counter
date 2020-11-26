e7e339171acca028cd5f179e74abb734b27f50b0
Jacob Bruér <bruer.pj@gmail.com>
2020-11-26 09:36:53 +0100

    added mockito to pom and updated integration testing suites

---
+++ src/test/java/se/pjbruer/charactercounter/ApiTests.java
@@ -0,0 +1,141 @@
+package se.pjbruer.charactercounter;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import se.pjbruer.charactercounter.service.CharacterCounterService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CharacterCounterService counterService;
+
+    // HAPPY CASES
+
+    @Test
+    void shouldAcceptParamsAndReturnResultAndHttp200() throws Exception {
+        String text = "abba rosor apa sms aha bob";
+        Character character = 'a';
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/findWordsParams")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("text", text)
+                .param("character", String.valueOf(character));
+
+        MvcResult result = mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals("{\"result\":3}", result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void shouldAcceptBodyAndReturnResultAndHttp200() throws Exception {
+        String json = "{\"text\":\"abba rosor apa sms aha bob\", \"character\":\"a\"}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/findWordsBody")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        MvcResult result = mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals("{\"result\":3}", result.getResponse().getContentAsString());
+    }
+
+    // SAD CASES
+
+    @Test
+    void shouldNotAcceptParamsAndReturnHttp400() throws Exception {
+        String text = "abba rosor apa sms aha bob";
+        Character character = 'a';
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/findWordsParams")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("text", text)
+                .param("wrongParam", String.valueOf(character));
+
+        MvcResult result = mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        assertEquals(400, result.getResponse().getStatus());
+    }
+
+    @Test
+    void shouldNotAcceptBodyAndReturnHttp400() throws Exception {
+        String brokenJson = "\"text\":\"abba rosor apa sms aha bob\", \"character\":\"a\"}";
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/findWordsBody")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(brokenJson);
+
+        MvcResult result = mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        assertEquals(400, result.getResponse().getStatus());
+    }
+
+    @Test
+    void shouldNotAcceptParamPathAndReturnHttp404() throws Exception {
+        String text = "abba rosor apa sms aha bob";
+        Character character = 'a';
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/notRightPath")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("text", text)
+                .param("character", String.valueOf(character));
+
+        MvcResult result = mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        assertEquals(404, result.getResponse().getStatus());
+    }
+
+    @Test
+    void shouldNotAcceptBodyPathAndReturnHttp404() throws Exception {
+        String json = "{\"text\":\"abba rosor apa sms aha bob\", \"character\":\"a\"}";
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/notRightPath")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        MvcResult result = mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        assertEquals(404, result.getResponse().getStatus());
+    }
+}
--- src/test/java/se/pjbruer/charactercounter/CharacterCounterControllerIntegrationTests.java
+++ src/test/java/se/pjbruer/charactercounter/CharacterCounterControllerIntegrationTests.java
@@ -1,141 +1,51 @@
 package se.pjbruer.charactercounter;
 
-
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import se.pjbruer.charactercounter.controller.CharacterCounterController;
+import se.pjbruer.charactercounter.model.CharacterCounterResponse;
 import se.pjbruer.charactercounter.service.CharacterCounterService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Objects;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class CharacterCounterControllerIntegrationTests {
 
-    @Autowired
-    private MockMvc mvc;
+    @InjectMocks
+    CharacterCounterController characterCounterController;
 
-    @Autowired
-    private CharacterCounterService counterService;
+    @Mock
+    CharacterCounterService characterCounterService;
 
-    // HAPPY CASES
+
+    @Before
+    public void setup() throws Exception {
+        characterCounterService = Mockito.mock(CharacterCounterService.class);
+        characterCounterController = new CharacterCounterController(characterCounterService);
+
+    }
 
     @Test
-    void shouldAcceptParamsAndReturnResultAndHttp200() throws Exception {
+    void shouldReturnCharacterCounterResponseFromService() {
+
         String text = "abba rosor apa sms aha bob";
         Character character = 'a';
 
-        RequestBuilder request = MockMvcRequestBuilders
-                .post("/api/findWordsParams")
-                .contentType(MediaType.APPLICATION_JSON)
-                .param("text", text)
-                .param("character", String.valueOf(character));
+        CharacterCounterResponse response = new CharacterCounterResponse(3);
 
-        MvcResult result = mvc.perform(request)
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn();
+        Mockito.when(characterCounterService.findWords(text, character)).thenReturn(response);
 
-        assertEquals("{\"result\":3}", result.getResponse().getContentAsString());
-    }
+        ResponseEntity<CharacterCounterResponse> result = characterCounterController.findWords(text, character);
 
-    @Test
-    void shouldAcceptBodyAndReturnResultAndHttp200() throws Exception {
-        String json = "{\"text\":\"abba rosor apa sms aha bob\", \"character\":\"a\"}";
-        RequestBuilder request = MockMvcRequestBuilders
-                .post("/api/findWordsBody")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json);
+        assertEquals(3, Objects.requireNonNull(result.getBody()).getResult());
 
-        MvcResult result = mvc.perform(request)
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn();
-
-        assertEquals("{\"result\":3}", result.getResponse().getContentAsString());
-    }
-
-    // SAD CASES
-
-    @Test
-    void shouldNotAcceptParamsAndReturnHttp400() throws Exception {
-        String text = "abba rosor apa sms aha bob";
-        Character character = 'a';
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .post("/api/findWordsParams")
-                .contentType(MediaType.APPLICATION_JSON)
-                .param("text", text)
-                .param("wrongParam", String.valueOf(character));
-
-        MvcResult result = mvc.perform(request)
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andReturn();
-
-        assertEquals(400, result.getResponse().getStatus());
-    }
-
-    @Test
-    void shouldNotAcceptBodyAndReturnHttp400() throws Exception {
-        String brokenJson = "\"text\":\"abba rosor apa sms aha bob\", \"character\":\"a\"}";
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .post("/api/findWordsBody")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(brokenJson);
-
-        MvcResult result = mvc.perform(request)
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andReturn();
-
-        assertEquals(400, result.getResponse().getStatus());
-    }
-
-    @Test
-    void shouldNotAcceptParamPathAndReturnHttp404() throws Exception {
-        String text = "abba rosor apa sms aha bob";
-        Character character = 'a';
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .post("/api/notRightPath")
-                .contentType(MediaType.APPLICATION_JSON)
-                .param("text", text)
-                .param("character", String.valueOf(character));
-
-        MvcResult result = mvc.perform(request)
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andReturn();
-
-        assertEquals(404, result.getResponse().getStatus());
-    }
-
-    @Test
-    void shouldNotAcceptBodyPathAndReturnHttp404() throws Exception {
-        String json = "{\"text\":\"abba rosor apa sms aha bob\", \"character\":\"a\"}";
-        RequestBuilder request = MockMvcRequestBuilders
-                .post("/api/notRightPath")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json);
-
-        MvcResult result = mvc.perform(request)
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andReturn();
-
-        assertEquals(404, result.getResponse().getStatus());
     }
 }
